44166abbbed1ef4ab978eac69c5342a6f1c58b6d
Harry Zheng <harry@harryzheng.com>
2022-03-10 22:04:40 -0500

    [format] Improve coverage

--- internal/util/format/format_test.go
+++ internal/util/format/format_test.go
@@ -1,1 +1,73 @@
 package format
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatTypeYearMonth(t *testing.T) {
+	tests := []struct {
+		emailType   string
+		emailTime   time.Time
+		expected    string
+		expectedErr error
+	}{
+		{
+			"inbox", time.Date(2022, 3, 10, 21, 57, 52, 0, time.UTC),
+			"inbox#2022-03", nil,
+		},
+		{
+			"sent", time.Date(2021, 9, 10, 21, 57, 52, 0, time.UTC),
+			"sent#2021-09", nil,
+		},
+		{
+			"invalid", time.Date(2021, 9, 10, 21, 57, 52, 0, time.UTC),
+			"", ErrInvalidEmailType,
+		},
+	}
+
+	for _, test := range tests {
+		actual, err := FormatTypeYearMonth(test.emailType, test.emailTime)
+		assert.Equal(t, test.expected, actual)
+		assert.Equal(t, test.expectedErr, err)
+	}
+}
+
+func TestFormatDateTime(t *testing.T) {
+	tests := []struct {
+		emailTime time.Time
+		expected  string
+	}{
+		{
+			time.Date(2022, 3, 10, 21, 57, 52, 0, time.UTC),
+			"10-21:57:52",
+		},
+		{
+			time.Date(2021, 9, 10, 21, 00, 00, 0, time.UTC),
+			"10-21:00:00",
+		},
+	}
+
+	for _, test := range tests {
+		actual := FormatDateTime(test.emailTime)
+		assert.Equal(t, test.expected, actual)
+	}
+}
+
+func TestRejoinDate(t *testing.T) {
+	tests := []struct {
+		ym       string
+		dt       string
+		expected string
+	}{
+		{"2022-03", "10-21:00:00", "2022-03-10T21:00:00Z"},
+		{"2021-09", "10-21:57:52", "2021-09-10T21:57:52Z"},
+	}
+
+	for _, test := range tests {
+		actual := RejoinDate(test.ym, test.dt)
+		assert.Equal(t, test.expected, actual)
+	}
+}
