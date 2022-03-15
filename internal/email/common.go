495d33c8fead5ed91c36342ca34a2f5c11726c6b
Harry Zheng <harry@harryzheng.com>
2022-03-15 17:30:53 -0400

    [email] Add comments for exported types

--- internal/email/common.go
+++ internal/email/common.go
@@ -7,19 +7,23 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
+// QueryAPI defines set of API required to query for emails
 type QueryAPI interface {
 	Query(ctx context.Context, params *dynamodb.QueryInput, optFns ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error)
 }
 
+// GetItemAPI defines set of API required to get an email
 type GetItemAPI interface {
 	GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error)
 }
 
+// DeleteItemAPI defines set of API required to delete an email
 type DeleteItemAPI interface {
 	DeleteItem(ctx context.Context, params *dynamodb.DeleteItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.DeleteItemOutput, error)
 	DeleteObject(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectOutput, error)
 }
 
+// UpdateItemAPI defines set of API required to update an email
 type UpdateItemAPI interface {
 	UpdateItem(ctx context.Context, params *dynamodb.UpdateItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.UpdateItemOutput, error)
 }
