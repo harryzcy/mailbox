797bdcda2c27421d3da37089dbfad5e1b4ad9ee3
Harry Zheng <harry@harryzheng.com>
2022-03-16 18:17:31 -0400

    [email] Limit trash & untrash to inbox emails

--- internal/email/untrash.go
+++ internal/email/untrash.go
@@ -18,7 +18,10 @@ func Untrash(ctx context.Context, api UpdateItemAPI, messageID string) error {
 			"MessageID": &types.AttributeValueMemberS{Value: messageID},
 		},
 		UpdateExpression:    aws.String("REMOVE TrashedTime"),
-		ConditionExpression: aws.String("attribute_exists(TrashedTime)"),
+		ConditionExpression: aws.String("attribute_exists(TrashedTime) AND begins_with(TypeYearMonth, :v_type)"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":v_type": &types.AttributeValueMemberS{Value: EmailTypeInbox},
+		},
 	})
 	if err != nil {
 		var condFailedErr *types.ConditionalCheckFailedException
