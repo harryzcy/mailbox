2f80e633792b90e59546b6fb05853b2352f324e5
Harry Zheng <harry@harryzheng.com>
2022-03-19 22:32:41 -0400

    [script] Update script

--- script/config-example-gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-current=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-base_dir=$(dirname ${current})
-cd ${base_dir}
-
-cp serverless.yml serverless.yml.example
-sed -i '' 's/S3_BUCKET: your-mailbox/S3_BUCKET: example-mailbox # set this to your S3 bucket name/' serverless.yml.example
-sed -i '' 's/SQS_QUEUE: your-mailbox/SQS_QUEUE: example-mailbox # set this to your SQS queue name/' serverless.yml.example
