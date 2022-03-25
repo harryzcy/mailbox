9592a7979c5e232758e384b80fd13eda856c4b87
Harry Zheng <harry@harryzheng.com>
2022-03-25 03:51:17 -0400

    [avutil] Support encoding attribute values

---
+++ internal/util/avutil/encode.go
@@ -0,0 +1,139 @@
+package avutil
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+func EncodeAttributeValue(value types.AttributeValue) []byte {
+	var content []byte
+
+	switch value.(type) {
+	case *types.AttributeValueMemberB:
+		content = EncodeAttributeValueB(value)
+
+	case *types.AttributeValueMemberBOOL:
+		content = EncodeAttributeValueBOOL(value)
+
+	case *types.AttributeValueMemberBS:
+		content = EncodeAttributeValueBS(value)
+
+	case *types.AttributeValueMemberL:
+		content = EncodeAttributeValueL(value)
+
+	case *types.AttributeValueMemberM:
+		content = EncodeAttributeValueM(value)
+
+	case *types.AttributeValueMemberN:
+		content = EncodeAttributeValueN(value)
+
+	case *types.AttributeValueMemberNS:
+		content = EncodeAttributeValueNS(value)
+
+	case *types.AttributeValueMemberNULL:
+		content = EncodeAttributeValueNULL(value)
+
+	case *types.AttributeValueMemberS:
+		content = EncodeAttributeValueS(value)
+
+	case *types.AttributeValueMemberSS:
+		content = EncodeAttributeValueSS(value)
+
+	}
+	return content
+}
+
+func EncodeAttributeValueB(value types.AttributeValue) []byte {
+	encoded := base64.StdEncoding.EncodeToString(value.(*types.AttributeValueMemberB).Value)
+	content := fmt.Sprintf("{\"B\":\"%s\"}", encoded)
+	return []byte(content)
+}
+
+func EncodeAttributeValueBOOL(value types.AttributeValue) []byte {
+	content := fmt.Sprintf("{\"BOOL\":%t}", value.(*types.AttributeValueMemberBOOL).Value)
+	return []byte(content)
+}
+
+func EncodeAttributeValueBS(value types.AttributeValue) []byte {
+	result := []byte("{\"BS\":[")
+	for i, item := range value.(*types.AttributeValueMemberBS).Value {
+		if i != 0 {
+			result = append(result, ',')
+		}
+		result = append(result, '"')
+		encoded := base64.StdEncoding.EncodeToString(item)
+		result = append(result, encoded...)
+		result = append(result, '"')
+	}
+	result = append(result, []byte("]}")...)
+	return result
+}
+
+func EncodeAttributeValueL(value types.AttributeValue) []byte {
+	result := []byte("{\"L\":[")
+	for _, v := range value.(*types.AttributeValueMemberL).Value {
+		content := EncodeAttributeValue(v)
+		result = append(result, content...)
+	}
+	result = append(result, ']', '}')
+	return result
+}
+
+func EncodeAttributeValueM(value types.AttributeValue) []byte {
+	result := []byte("{\"M\":{")
+
+	for k, v := range value.(*types.AttributeValueMemberM).Value {
+		result = append(result, []byte("\""+k+"\":")...)
+
+		content := EncodeAttributeValue(v)
+		result = append(result, content...)
+	}
+
+	result = append(result, '}', '}')
+	return result
+}
+
+func EncodeAttributeValueN(value types.AttributeValue) []byte {
+	content := fmt.Sprintf("{\"N\":\"%s\"}", value.(*types.AttributeValueMemberN).Value)
+	return []byte(content)
+}
+
+func EncodeAttributeValueNS(value types.AttributeValue) []byte {
+	result := []byte("{\"NS\":[")
+	for i, item := range value.(*types.AttributeValueMemberNS).Value {
+		if i != 0 {
+			result = append(result, ',')
+		}
+		result = append(result, '"')
+		result = append(result, item...)
+		result = append(result, '"')
+	}
+	result = append(result, ']', '}')
+
+	return result
+}
+
+func EncodeAttributeValueNULL(value types.AttributeValue) []byte {
+	return []byte("{\"NULL\":true}")
+}
+
+func EncodeAttributeValueS(value types.AttributeValue) []byte {
+	return []byte("{\"S\":\"" + value.(*types.AttributeValueMemberS).Value + "\"}")
+}
+
+func EncodeAttributeValueSS(value types.AttributeValue) []byte {
+	result := []byte("{\"SS\":[")
+	for i, item := range value.(*types.AttributeValueMemberSS).Value {
+		if i != 0 {
+			result = append(result, ',')
+		}
+		result = append(result, '"')
+		result = append(result, item...)
+		result = append(result, '"')
+	}
+	result = append(result, ']', '}')
+
+	return result
+}
---
+++ internal/util/avutil/encode_test.go
@@ -0,0 +1,74 @@
+package avutil
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncodeAttributeValue(t *testing.T) {
+	tests := []struct {
+		in       types.AttributeValue
+		expected []byte
+	}{
+		{
+			in:       &types.AttributeValueMemberB{Value: []byte("this text is base64-encoded")},
+			expected: []byte("{\"B\":\"dGhpcyB0ZXh0IGlzIGJhc2U2NC1lbmNvZGVk\"}"),
+		},
+		{
+			in:       &types.AttributeValueMemberBOOL{Value: true},
+			expected: []byte("{\"BOOL\":true}"),
+		},
+		{
+			in:       &types.AttributeValueMemberBS{Value: [][]byte{[]byte("Sunny"), []byte("Rainy"), []byte("Snowy")}},
+			expected: []byte("{\"BS\":[\"U3Vubnk=\",\"UmFpbnk=\",\"U25vd3k=\"]}"),
+		},
+		{
+			in:       &types.AttributeValueMemberS{Value: "foo"},
+			expected: []byte("{\"S\":\"foo\"}"),
+		},
+		{
+			in: &types.AttributeValueMemberL{Value: []types.AttributeValue{
+				&types.AttributeValueMemberS{Value: "foo"},
+			}},
+			expected: []byte("{\"L\":[{\"S\":\"foo\"}]}"),
+		},
+		{
+			in: &types.AttributeValueMemberM{
+				Value: map[string]types.AttributeValue{
+					"foo": &types.AttributeValueMemberS{Value: "bar"},
+				},
+			},
+			expected: []byte("{\"M\":{\"foo\":{\"S\":\"bar\"}}}"),
+		},
+		{
+			in:       &types.AttributeValueMemberN{Value: "123.45"},
+			expected: []byte("{\"N\":\"123.45\"}"),
+		},
+		{
+			in:       &types.AttributeValueMemberNS{Value: []string{"123.45", "123.45"}},
+			expected: []byte("{\"NS\":[\"123.45\",\"123.45\"]}"),
+		},
+		{
+			in:       &types.AttributeValueMemberNULL{Value: true},
+			expected: []byte("{\"NULL\":true}"),
+		},
+		{
+			in:       &types.AttributeValueMemberS{Value: "foo"},
+			expected: []byte("{\"S\":\"foo\"}"),
+		},
+		{
+			in:       &types.AttributeValueMemberSS{Value: []string{"foo", "bar"}},
+			expected: []byte("{\"SS\":[\"foo\",\"bar\"]}"),
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual := EncodeAttributeValue(test.in)
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
