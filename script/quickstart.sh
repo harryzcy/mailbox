71c7070696fa8b5c3aae3db74e4b23552575e3d1
Chongyi Zheng <harry@harryzheng.com>
2022-04-08 18:10:46 -0400

    [script] Generate config file with sqs options

--- script/quickstart.sh
+++ script/quickstart.sh
@@ -7,7 +7,12 @@ cd ${base_dir}
 # parse arguments
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -f|--file)
+    -s|--serverless)
+      SERVERLESS_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -c|--config)
       CONFIG_FILE="$2"
       shift # past argument
       shift # past value
@@ -19,9 +24,13 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-# default config file is 'serverless.yml'
+# default config files
+if [ -z "${SERVERLESS_FILE}" ]; then
+  SERVERLESS_FILE="serverless.yml"
+fi
+
 if [ -z "${CONFIG_FILE}" ]; then
-  CONFIG_FILE="serverless.yml"
+  CONFIG_FILE="config.yml"
 fi
 
 
@@ -49,13 +58,35 @@ while [[ -z "${sqs_queue}" ]]; do
     read -p "Enter SQS queue name: " sqs_queue
 done
 
-echo "About to write to ${CONFIG_FILE}:"
+# Enable SQS
+while [[ -z "${sqs_enabled}" ]] || [[ "${sqs_enabled}" != "y" && "${sqs_enabled}" != "n" ]]; do
+    read -p "Enable SQS? (Y/n): " sqs_enabled
+    if [[ -z "${sqs_enabled}" ]] || [[ "${sqs_enabled}" == "Y" ]]; then
+        sqs_enabled="y"
+    elif [[ "${sqs_enabled}" == "N" ]]; then
+        sqs_enabled="n"
+    fi
+done
+
+function boolean() {
+  case $1 in
+    TRUE) echo true ;;
+    FALSE) echo false ;;
+    y) echo true ;;
+    n) echo false ;;
+    *) echo "Err: Unknown boolean value \"$1\"" 1>&2; exit 1 ;;
+   esac
+}
+
+echo
+echo "About to write to ${SERVERLESS_FILE} and ${CONFIG_FILE}:"
 echo
 
 echo "stage: ${stage}"
 echo "region: ${region}"
 echo "S3 bucket name: ${s3_bucket}"
 echo "SQS queue name: ${sqs_queue}"
+echo "SQS enabled: $(boolean ${sqs_enabled})"
 
 echo
 read -p "Is this correct? (Y/n)" answer
@@ -69,10 +100,17 @@ if [[ "${answer}" != "Y" ]]; then
     exit 1
 fi
 
-cp serverless.yml.example ${CONFIG_FILE}
-perl -i -pe"s/dev/${stage}/g" ${CONFIG_FILE}
-perl -i -pe"s/us-west-2/${region}/g" ${CONFIG_FILE}
-perl -i -pe"s/example-mailbox # set this to your S3 bucket name/${s3_bucket}/g" ${CONFIG_FILE}
-perl -i -pe"s/example-mailbox # set this to your SQS queue name/${sqs_queue}/g" ${CONFIG_FILE}
+cp serverless.yml.example ${SERVERLESS_FILE}
+perl -i -pe"s/dev/${stage}/g" ${SERVERLESS_FILE}
+perl -i -pe"s/us-west-2/${region}/g" ${SERVERLESS_FILE}
+perl -i -pe"s/example-mailbox # set this to your S3 bucket name/${s3_bucket}/g" ${SERVERLESS_FILE}
+perl -i -pe"s/example-mailbox # set this to your SQS queue name/${sqs_queue}/g" ${SERVERLESS_FILE}
+
+
+cat << EOT > ${CONFIG_FILE}
+sqs:
+  enabled: $(boolean ${sqs_enabled})
+EOT
 
 echo "Done."
+echo "Please review the config files and run 'make deploy' to deploy."
