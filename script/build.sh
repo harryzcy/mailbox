0fde1c69a04da19e6389a71911c2880ad14d26d4
harryzcy <harry@harryzheng.com>
2022-07-16 16:16:58 -0400

    Create build script

---
+++ script/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apiFuncs=("list" "get" "trash" "untrash" "delete" "create" "save" "send")
+
+for i in ${!apiFuncs[@]};
+do
+  func=${apiFuncs[$i]}
+  env GOOS=linux go build -ldflags="-s -w" -o bin/api/emails/${func} api/emails/${func}/*
+done
+
+env GOOS=linux go build -ldflags="-s -w" -o bin/functions/emailReceive functions/emailReceive/*
