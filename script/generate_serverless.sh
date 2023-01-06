5691017b9e5d597b2f69064dfcf7ff1faede4065
Chongyi Zheng <harry@harryzheng.com>
2023-01-05 23:13:13 -0500

    Migrate to amazon linux 2 runtime (#197)
    * Use provided.al2 runtime
    
    * Update config generation script
    
    * Fix escapes
    
    * Quote to fix the lint
    
    * One more quote
    
    * Another escape

--- script/generate_serverless.sh
+++ script/generate_serverless.sh
@@ -12,14 +12,14 @@ function generate() {
   sqs_queue=${8:-sqs_queue}
   auth_method=${9:-iam}
 
-  cat << EOT > ${filename}
+  cat << EOT > "${filename}"
 service: ${service}
 
 frameworkVersion: '3'
 
 provider:
   name: aws
-  runtime: go1.x
+  runtime: provided.al2
   memorySize: 128
   stage: \${opt:stage, '${stage}'}
   region: \${opt:region, '${region}'}
@@ -31,12 +31,12 @@ provider:
 EOT
 
   if [[ "${sqs_enabled}" == "true" ]]; then
-    cat << EOT >> ${filename}
+    cat << EOT >> "${filename}"
     SQS_QUEUE: ${sqs_queue}
 EOT
   fi
 
-  cat << EOT >> ${filename}
+  cat << EOT >> "${filename}"
   iam:
     role:
       statements:
@@ -57,9 +57,13 @@ EOT
             - s3:GetObject
             - s3:DeleteObject
           Resource: "arn:aws:s3::*:\${self:provider.environment.S3_BUCKET}/*"
+        - Effect: Allow
+          Action:
+            - ses:SendEmail
+          Resource: "arn:aws:ses:\${self:provider.region}:*:identity/*"
 EOT
   if [[ "${sqs_enabled}" == "true" ]]; then
-  cat << EOT >> ${filename}
+  cat << EOT >> "${filename}"
         - Effect: Allow
           Action:
             - sqs:GetQueueUrl
@@ -68,14 +72,14 @@ EOT
 EOT
   fi
 
-  cat << EOT >> ${filename}
+  cat << EOT >> "${filename}"
   apiGateway:
     shouldStartNameWithService: true
 
 package:
   patterns:
     - '!./**'
-    - './bin/**'
+  individually: true
 EOT
 
   if [[ "${auth_method}" == "iam" ]]; then
@@ -86,58 +90,78 @@ EOT
     auth_statement=""
   fi
 
-  cat << EOT >> ${filename}
+  cat << EOT >> "${filename}"
 functions:
   emailReceive:
-    handler: bin/functions/emailReceive
+    handler: bootstrap
+    environment:
+      ENABLE_SQS: true
+    package:
+      artifact: bin/emailReceive.zip
   emailsList:
     handler: bin/api/emails/list
     events:
       - httpApi:
           path: /emails
           method: GET${auth_statement}
+    package:
+      artifact: bin/list.zip
   emailsGet:
-    handler: bin/api/emails/get
+    handler: bootstrap
     events:
       - httpApi:
           method: GET
           path: /emails/{messageID}${auth_statement}
+    package:
+      artifact: bin/get.zip
   emailsTrash:
-    handler: bin/api/emails/trash
+    handler: bootstrap
     events:
       - httpApi:
           method: POST
           path: /emails/{messageID}/trash${auth_statement}
+    package:
+      artifact: bin/trash.zip
   emailsUntrash:
-    handler: bin/api/emails/untrash
+    handler: bootstrap
     events:
       - httpApi:
           method: POST
           path: /emails/{messageID}/untrash${auth_statement}
+    package:
+      artifact: bin/untrash.zip
   emailsDelete:
-    handler: bin/api/emails/delete
+    handler: bootstrap
     events:
       - httpApi:
           method: DELETE
           path: /emails/{messageID}${auth_statement}
+    package:
+      artifact: bin/delete.zip
   emailsCreate:
-    handler: bin/api/emails/create
+    handler: bootstrap
     events:
       - httpApi:
           method: POST
           path: /emails${auth_statement}
+    package:
+      artifact: bin/create.zip
   emailsSave:
-    handler: bin/api/emails/save
+    handler: bootstrap
     events:
       - httpApi:
           method: PUT
           path: /emails/{messageID}${auth_statement}
+    package:
+      artifact: bin/save.zip
   emailsSend:
-    handler: bin/api/emails/send
+    handler: bootstrap
     events:
       - httpApi:
           method: POST
           path: /emails/{messageID}/send${auth_statement}
+    package:
+      artifact: bin/send.zip
 
 resources:
   Resources:
@@ -169,6 +193,8 @@ resources:
               ProjectionType: INCLUDE
               NonKeyAttributes:
                 - Subject
+                - From
+                - To
             ProvisionedThroughput:
               ReadCapacityUnits: 3
               WriteCapacityUnits: 1
