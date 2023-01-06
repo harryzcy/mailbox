209a10a3975fa657f26919aefad4c7abf4fb3d46
harryzcy <harry@harryzheng.com>
2023-01-05 22:42:18 -0500

    Use provided.al2 runtime

--- script/build.sh
+++ script/build.sh
@@ -6,6 +6,11 @@ for i in "${!apiFuncs[@]}";
 do
   func="${apiFuncs[$i]}"
   env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bin/api/emails/"${func}" api/emails/"${func}"/*
+  cp bin/api/emails/"${func}" bin/bootstrap
+  zip -j bin/"${func}".zip bin/bootstrap
 done
 
 env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bin/functions/emailReceive functions/emailReceive/*
+cp bin/functions/emailReceive bin/bootstrap
+zip -j bin/emailReceive.zip bin/bootstrap
+rm bin/bootstrap
