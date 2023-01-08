30189f1fa75e245c8e4773c58796410980fbfdc9
harryzcy <harry@harryzheng.com>
2023-01-08 12:25:36 -0500

    Support getting raw email from s3

---
+++ api/emails/getRaw/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/harryzcy/mailbox/internal/datasource/storage"
+	"github.com/harryzcy/mailbox/internal/email"
+	"github.com/harryzcy/mailbox/internal/util/apiutil"
+)
+
+// AWS Region
+var region = os.Getenv("REGION")
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (apiutil.Response, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	fmt.Println("request received")
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		fmt.Printf("unable to load SDK config, %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+
+	messageID := req.PathParameters["messageID"]
+	fmt.Printf("request params: [messagesID] %s\n", messageID)
+
+	if messageID == "" {
+		return apiutil.NewErrorResponse(http.StatusBadRequest, "bad request: invalid messageID"), nil
+	}
+
+	result, err := storage.S3.GetEmailRaw(ctx, s3.NewFromConfig(cfg), messageID)
+	if err != nil {
+		if err == email.ErrNotFound {
+			fmt.Println("email not found")
+			return apiutil.NewErrorResponse(http.StatusNotFound, "email not found"), nil
+		}
+		if err == email.ErrTooManyRequests {
+			fmt.Println("too many requests")
+			return apiutil.NewErrorResponse(http.StatusTooManyRequests, "too many requests"), nil
+		}
+		fmt.Printf("get raw email failed: %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+
+	body, err := json.Marshal(result)
+	if err != nil {
+		fmt.Printf("marshal failed: %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+	fmt.Println("invoke successful")
+	return apiutil.NewSuccessJSONResponse(string(body)), nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
