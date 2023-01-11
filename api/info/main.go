2433928e3121b44ab552fe881bd5a4a1d3aa3891
harryzcy <harry@harryzheng.com>
2023-01-10 21:40:28 -0500

    Add `/info` route to return build info

---
+++ api/info/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/harryzcy/mailbox/internal/util/apiutil"
+)
+
+var (
+	version   = "dev"
+	commit    = "n/a"
+	buildDate = "n/a"
+)
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (apiutil.Response, error) {
+	body, err := json.Marshal(map[string]string{
+		"version": version,
+		"commit":  commit,
+		"build":   buildDate,
+	})
+	if err != nil {
+		fmt.Printf("marshal failed: %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+	return apiutil.NewSuccessJSONResponse(string(body)), nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
