c6fec4f95fac1795ecc79a3048562273751a6911
harryzcy <harry@harryzheng.com>
2023-02-20 02:45:29 -0500

    Test basic threading

--- integration/thread_test.go
+++ integration/thread_test.go
@@ -39,7 +39,7 @@ func testStoreEmails_NoThread(t *testing.T) {
 	email.StoreEmail(context.TODO(), client, &email.StoreEmailInput{
 		Item: map[string]types.AttributeValue{
 			"MessageID":         &types.AttributeValueMemberS{Value: "1"},
-			"OriginalMessageID": &types.AttributeValueMemberS{Value: "3@example.com"},
+			"OriginalMessageID": &types.AttributeValueMemberS{Value: "1@example.com"},
 			"TypeYearMonth":     &types.AttributeValueMemberS{Value: "inbox#2023-01"},
 			"DateTime":          &types.AttributeValueMemberS{Value: "01T00:00:00Z"},
 		},
@@ -57,9 +57,106 @@ func testStoreEmails_NoThread(t *testing.T) {
 	email.StoreEmail(context.TODO(), client, &email.StoreEmailInput{
 		Item: map[string]types.AttributeValue{
 			"MessageID":         &types.AttributeValueMemberS{Value: "3"},
-			"OriginalMessageID": &types.AttributeValueMemberS{Value: "non-exist@example.com"},
+			"OriginalMessageID": &types.AttributeValueMemberS{Value: "3@example.com"},
 			"TypeYearMonth":     &types.AttributeValueMemberS{Value: "inbox#2023-01"},
 			"DateTime":          &types.AttributeValueMemberS{Value: "01T00:00:00Z"},
 		},
 	})
+
+	testItemExists(t, "1")
+	testItemExists(t, "2")
+	testItemExists(t, "3")
+}
+
+func testStoreEmails_BasicThread(t *testing.T) {
+	defer deleteAllItems()
+
+	if num := testEmptyTable(t); num != 0 {
+		return
+	}
+
+	email.StoreEmail(context.TODO(), client, &email.StoreEmailInput{
+		Item: map[string]types.AttributeValue{
+			"MessageID":         &types.AttributeValueMemberS{Value: "1"},
+			"OriginalMessageID": &types.AttributeValueMemberS{Value: "1@example.com"},
+			"TypeYearMonth":     &types.AttributeValueMemberS{Value: "inbox#2023-01"},
+			"DateTime":          &types.AttributeValueMemberS{Value: "01T00:00:00Z"},
+			"Subject":           &types.AttributeValueMemberS{Value: "Subject 1"},
+		},
+	})
+
+	// should create a new thread
+	email.StoreEmail(context.TODO(), client, &email.StoreEmailInput{
+		Item: map[string]types.AttributeValue{
+			"MessageID":         &types.AttributeValueMemberS{Value: "2"},
+			"OriginalMessageID": &types.AttributeValueMemberS{Value: "2@example.com"},
+			"TypeYearMonth":     &types.AttributeValueMemberS{Value: "inbox#2023-01"},
+			"DateTime":          &types.AttributeValueMemberS{Value: "01T00:00:00Z"},
+			"Subject":           &types.AttributeValueMemberS{Value: "Subject 2"},
+		},
+		InReplyTo:  "1@example.com",
+		References: "1@example.com",
+	})
+
+	// should add to the same thread
+	email.StoreEmail(context.TODO(), client, &email.StoreEmailInput{
+		Item: map[string]types.AttributeValue{
+			"MessageID":         &types.AttributeValueMemberS{Value: "3"},
+			"OriginalMessageID": &types.AttributeValueMemberS{Value: "3@example.com"},
+			"TypeYearMonth":     &types.AttributeValueMemberS{Value: "inbox#2023-01"},
+			"DateTime":          &types.AttributeValueMemberS{Value: "01T00:00:00Z"},
+			"Subject":           &types.AttributeValueMemberS{Value: "Subject 3"},
+		},
+		InReplyTo:  "2@example.com",
+		References: "2@example.com",
+	})
+
+	testItemExists(t, "1")
+	testItemExists(t, "2")
+	testItemExists(t, "3")
+
+	resp, err := client.Scan(context.TODO(), &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	})
+	assert.NoError(t, err)
+
+	assert.Equal(t, 4, len(resp.Items))
+
+	threadID := ""
+	for _, item := range resp.Items {
+		messageID := item["MessageID"].(*types.AttributeValueMemberS).Value
+		if len(messageID) == 32 {
+			// is thread
+			threadID = messageID
+
+			ids := item["EmailIDs"].(*types.AttributeValueMemberL).Value
+			assert.Equal(t, 3, len(ids))
+			assert.Equal(t, "1", ids[0].(*types.AttributeValueMemberS).Value)
+			assert.Equal(t, "2", ids[1].(*types.AttributeValueMemberS).Value)
+			assert.Equal(t, "3", ids[2].(*types.AttributeValueMemberS).Value)
+
+			assert.Equal(t, "Subject 1", item["Subject"].(*types.AttributeValueMemberS).Value)
+			break
+		}
+	}
+
+	for _, item := range resp.Items {
+		messageID := item["MessageID"].(*types.AttributeValueMemberS).Value
+		if len(messageID) == 32 {
+			// is thread
+			continue
+		}
+		assert.Equal(t, threadID, item["ThreadID"].(*types.AttributeValueMemberS).Value)
+	}
+}
+
+func testItemExists(t *testing.T, messageID string) {
+	resp, err := client.GetItem(context.TODO(), &dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]types.AttributeValue{
+			"MessageID": &types.AttributeValueMemberS{Value: messageID},
+		},
+	})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, resp.Item)
 }
