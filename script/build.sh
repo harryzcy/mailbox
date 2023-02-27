2e48808acabd80a6566615dccb6c4af838189e19
harryzcy <harry@harryzheng.com>
2023-02-26 23:58:57 -0500

    Update build script to support threads/get API

--- script/build.sh
+++ script/build.sh
@@ -6,24 +6,22 @@ BUILD_VERSION=$(git describe --tags --always)
 
 ENVIRONMENT="env GOOS=linux GOARCH=amd64"
 
-apiFuncs=("list" "get" "getRaw" "getContent" "read" "trash" "untrash" "delete" "create" "save" "send")
+apiFuncs=(
+  "emails/list" "emails/get" "emails/getRaw" "emails/getContent" "emails/read" "emails/trash" "emails/untrash" "emails/delete" "emails/create" "emails/save" "emails/send"
+  "threads/get"
+  "info"
+)
+
+mkdir -p bin/emails
+mkdir -p bin/threads
 
 for i in "${!apiFuncs[@]}";
 do
   func="${apiFuncs[$i]}"
-  ${ENVIRONMENT} go build -ldflags="-s -w" -o bin/api/emails/"${func}" api/emails/"${func}"/*
-  cp bin/api/emails/"${func}" bin/bootstrap
+  ${ENVIRONMENT} go build -ldflags="-s -w" -o bin/api/"${func}" api/"${func}"/*
+  cp bin/api/"${func}" bin/bootstrap
   zip -j bin/"${func}".zip bin/bootstrap
 done
-
-${ENVIRONMENT} go build -ldflags="-s -w \
-                                  -X 'main.version=${BUILD_VERSION}' \
-                                  -X 'main.commit=${BUILD_COMMIT}' \
-                                  -X 'main.buildDate=${BUILD_DATE}' \
-                                  " \
-                        -o bin/api/info api/info/*
-cp bin/api/info bin/bootstrap
-zip -j bin/info.zip bin/bootstrap
 
 ${ENVIRONMENT} go build -ldflags="-s -w" -o bin/functions/emailReceive functions/emailReceive/*
 cp bin/functions/emailReceive bin/bootstrap
