82483d700791cc7a04e023f60cb80f492bd03bf7
Chongyi Zheng <harry@harryzheng.com>
2023-03-06 14:55:57 -0500

    Add get thread API (#224)
    * Add threads get handler
    
    * Update build script to support threads/get API
    
    * Info handler should have build time constants
    
    * Zip filename should be unique even in different directories
    
    * Remove unused directories
    
    * Fix permission issue
    
    * Order emails in a thread

---
+++ api/threads/get/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/harryzcy/mailbox/internal/email"
+	"github.com/harryzcy/mailbox/internal/util/apiutil"
+)
+
+// AWS Region
+var region = os.Getenv("REGION")
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (apiutil.Response, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	fmt.Println("request received")
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		fmt.Printf("unable to load SDK config, %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+
+	threadID := req.PathParameters["threadID"]
+	fmt.Printf("request params: [messagesID] %s\n", threadID)
+
+	if threadID == "" {
+		return apiutil.NewErrorResponse(http.StatusBadRequest, "bad request: invalid threadID"), nil
+	}
+
+	result, err := email.GetThreadWithEmails(ctx, dynamodb.NewFromConfig(cfg), threadID)
+	if err != nil {
+		if err == email.ErrNotFound {
+			fmt.Println("thread not found")
+			return apiutil.NewErrorResponse(http.StatusNotFound, "thread not found"), nil
+		}
+		if err == email.ErrTooManyRequests {
+			fmt.Println("too many requests")
+			return apiutil.NewErrorResponse(http.StatusTooManyRequests, "too many requests"), nil
+		}
+		fmt.Printf("dynamodb get failed: %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+
+	body, err := json.Marshal(result)
+	if err != nil {
+		fmt.Printf("marshal failed: %v\n", err)
+		return apiutil.NewErrorResponse(http.StatusInternalServerError, "internal error"), nil
+	}
+	fmt.Println("invoke successful")
+	return apiutil.NewSuccessJSONResponse(string(body)), nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
