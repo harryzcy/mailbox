388bd5b3f3e4774deddcbad6f049956bdc25cbca
Chongyi Zheng <git@zcy.dev>
2025-12-13 16:39:41 -0600

    Update aws provider and use variable for region

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-west-2"
+}
+
 provider "aws" {
-  region = "us-west-2"
+  region = var.aws_region
 }
