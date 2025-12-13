985ef66b44b546eeaf0190b1ab9df2048a8179ce
Chongyi Zheng <git@zcy.dev>
2025-12-13 16:22:11 -0600

    Bump versions

--- terraform.tf
+++ terraform.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92"
+      version = "~> 5.100"
     }
   }
 
-  required_version = ">= 1.2"
+  required_version = ">= 1.14"
 }
