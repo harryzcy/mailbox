6df008d8c84c2435d9de7fe2a773b2f06260a0e0
Chongyi Zheng <git@zcy.dev>
2026-01-18 00:32:31 -0600

    Move outputs to seperate file

--- main.tf
+++ main.tf
@@ -38,19 +38,3 @@ resource "aws_apigatewayv2_stage" "mailbox_api_default" {
     })
   }
 }
-
-
-output "api_endpoint" {
-  description = "Endpoint URL of the mailbox API Gateway"
-  value       = aws_apigatewayv2_api.mailbox_api.api_endpoint
-}
-
-output "api_id" {
-  description = "ID of the mailbox API Gateway"
-  value       = aws_apigatewayv2_api.mailbox_api.id
-}
-
-output "api_arn" {
-  description = "ARN of the mailbox API Gateway"
-  value       = aws_apigatewayv2_api.mailbox_api.arn
-}
