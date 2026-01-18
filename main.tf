2d6c941a4efe920524a49178ed22c99cf37056ed
Chongyi Zheng <git@zcy.dev>
2026-01-17 23:38:23 -0600

    Add outputs

--- main.tf
+++ main.tf
@@ -6,3 +6,18 @@ resource "aws_apigatewayv2_api" "mailbox_api" {
   name          = "${var.project_name}-${var.environment}"
   protocol_type = "HTTP"
 }
+
+output "api_endpoint" {
+  description = "Endpoint URL of the mailbox API Gateway"
+  value       = aws_apigatewayv2_api.mailbox_api.api_endpoint
+}
+
+output "api_id" {
+  description = "ID of the mailbox API Gateway"
+  value       = aws_apigatewayv2_api.mailbox_api.id
+}
+
+output "api_arn" {
+  description = "ARN of the mailbox API Gateway"
+  value       = aws_apigatewayv2_api.mailbox_api.arn
+}
