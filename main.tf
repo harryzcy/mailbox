1d5036f4599cea0404d22e763cc5d725140a8442
Chongyi Zheng <git@zcy.dev>
2026-01-17 23:28:47 -0600

    Add api gateway in terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_apigatewayv2_api" "mailbox_api" {
+  name          = "${var.project_name}-${var.environment}"
+  protocol_type = "HTTP"
+}
