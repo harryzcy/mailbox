a65db713139dc445124f09ef06b7fd3644e2add8
Chongyi Zheng <git@zcy.dev>
2026-01-18 00:07:48 -0600

    Add logs

--- main.tf
+++ main.tf
@@ -7,11 +7,36 @@ resource "aws_apigatewayv2_api" "mailbox_api" {
   protocol_type = "HTTP"
 }
 
+resource "aws_cloudwatch_log_group" "mailbox_api_access_logs" {
+  name = "/aws/apigateway/${var.project_name}-mailbox-api-access-logs"
+}
+
 resource "aws_apigatewayv2_stage" "mailbox_api_default" {
   api_id = aws_apigatewayv2_api.mailbox_api.id
   name   = "$default"
   # auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 100
+    throttling_rate_limit  = 50
+  }
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.mailbox_api_access_logs.arn
+    format = jsonencode({
+      requestId        = "$context.requestId"
+      ip               = "$context.identity.sourceIp"
+      requestTime      = "$context.requestTime"
+      httpMethod       = "$context.httpMethod"
+      path             = "$context.path"
+      status           = "$context.status"
+      protocol         = "$context.protocol"
+      responseLength   = "$context.responseLength"
+      integrationError = "$context.integrationErrorMessage"
+    })
+  }
 }
+
 
 output "api_endpoint" {
   description = "Endpoint URL of the mailbox API Gateway"
