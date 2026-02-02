aa05bfb25cb416f0836a1f3ee77afa8187c6be5e
Chongyi Zheng <git@zcy.dev>
2026-02-02 01:12:27 -0600

    Define http path

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ locals {
     info = {
       name       = "info"
       httpMethod = "GET"
+      httpPath   = "/info"
     }
   }
 }
@@ -111,7 +112,7 @@ resource "aws_apigatewayv2_integration" "integrations" {
 resource "aws_apigatewayv2_route" "routes" {
   for_each           = tomap(local.lambda_functions)
   api_id             = aws_apigatewayv2_api.mailbox_api.id
-  route_key          = "${each.value.httpMethod} /${each.key}"
+  route_key          = "${each.value.httpMethod} ${each.value.httpPath}"
   target             = "integrations/${aws_apigatewayv2_integration.integrations[each.key].id}"
   authorization_type = "AWS_IAM"
 }
