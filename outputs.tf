bf4e13f6bb07641c33ae6cf250f701669e8c7620
Chongyi Zheng <git@zcy.dev>
2026-08-22 20:21:07 -0500

    Add SQS queue for email notifications (#1178)

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,13 @@ output "api_arn" {
   description = "ARN of the mailbox API Gateway"
   value       = aws_apigatewayv2_api.mailbox_api.arn
 }
+
+output "sqs_queue_url" {
+  description = "URL of the email notification queue"
+  value       = aws_sqs_queue.notifications.url
+}
+
+output "sqs_queue_arn" {
+  description = "ARN of the email notification queue"
+  value       = aws_sqs_queue.notifications.arn
+}
